2487d13b3ff342cd77b34ee94a8427c534673237
donzyy <61470347+donzyy@users.noreply.github.com>
2024-02-13 13:26:03 +0000

    Modification
    *Added some API key's to the server.js

--- config/server.js
+++ config/server.js
@@ -1,8 +1,19 @@
-module.exports = ({ env }) => ({
+/* module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
+  },
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+  },
+});
+ */
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  app: {
+    keys: env.array('APP_KEYS', ['FEoF7AsEGH', 'YCK8ApuU4y']),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
