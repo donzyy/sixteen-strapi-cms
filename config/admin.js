d0f0e285f5977daf1a5c9ee6e6cad194c0d59c8e
donzyy <61470347+donzyy@users.noreply.github.com>
2024-02-13 14:20:53 +0000

    Modification
    * Modified the auth secreat, transfer token salt. Lets see what happens.

--- config/admin.js
+++ config/admin.js
@@ -1,13 +1,13 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET','NB/Rs9gfTf1oqMa0/uzT0w=='),
   },
   apiToken: {
     salt: env('API_TOKEN_SALT','H6ZRB5YTKJp4yLoS9x/y9Q=='),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env('TRANSFER_TOKEN_SALT','M+TWE0V+8wdrI15Lm3B2GQ=='),
     },
   },
   flags: {
